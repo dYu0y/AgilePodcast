3900a236439aeefd0d2ef3571d509a227f853661
yulin1506 <32516804+yulin1506@users.noreply.github.com>
2020-04-18 02:59:59 +0800

    第一個版本，UI待改進，播放功能基本完成，play list基本完成

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,25 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonList, IonCard, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import ReactAudioPlayer from 'react-audio-player';
+import React, { useState } from 'react';
+// import { pin, wifi, wine, warning, walk } from 'ionicons/icons';
 import './Home.css';
 
 const Home: React.FC = () => {
+  let musics: string[] = [
+    `埋葬 - 萤火虫の怨.mp3`,
+    `磁暴步兵羊永信 - Arcana Idola - 紅葉月梛葉.mp3`,
+    `磁暴步兵羊永信 - Cross†Soul - HyuN feat. Syepias.mp3`,
+    `磁暴步兵羊永信 - Ice Chandelier - pan.mp3`,
+    `磁暴步兵羊永信 - PUPA - モリモリあつし.mp3`,
+    `磁暴步兵羊永信 - 万吨匿名信 - 埋葬.mp3`
+  ];
+  let [song, changeSong] = useState(0);
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Poi</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent>
@@ -17,7 +28,19 @@ const Home: React.FC = () => {
             <IonTitle size="large">Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+        <IonList>
+          {musics.map((value: string, index: number) => <IonCard onClick={() => { changeSong(index) }}>{value}</IonCard>)}
+        </IonList>
+
+        
+        <IonList>
+          <IonCard>Now Playing...</IonCard>
+          <IonCard>{musics[song]}</IonCard>
+          <ReactAudioPlayer
+            src={[`assets\\` + musics[song]].join()}
+            controls
+          />
+        </IonList>
       </IonContent>
     </IonPage>
   );
